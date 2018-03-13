d7d9762ce80ffe9e5aadd70fcaa836bd1f69173d
OkBand <o.bandura42@gmail.com>
2018-03-13 23:53:25 +0300

    unsorted checking time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  var length = array.length;
+  var longestSequence = 0;
+  var i = 0;
+  var j =0;
+  while(i < length) {
+    var sequence = [];
+    sequence.push(array[i]);
+    while(array[j+1] === array[j] + 1){
+      sequence.push(array[j+1])
+      j++;
+    };
+    i = j + 1;
+    j++;
+    if (sequence.length > longestSequence) {
+      longestSequence = sequence.length;
+    };
+  };
+  return longestSequence;
 }
